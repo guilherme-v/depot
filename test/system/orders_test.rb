588af6e0212133489daa054405a5b3ea524b5264
guilherme <silva.guilherme09@gmail.com>
2020-02-02 22:05:10 -0300

    Task I: Sending e-mails and Processing with Background Jobs

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,40 +1,64 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit orders_url
-    assert_selector "h1", text: "Orders"
+    assert_selector 'h1', text: 'Orders'
   end
 
-  test "destroying a Order" do
+  test 'destroying a Order' do
     visit orders_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Order was successfully destroyed"
+    assert_text 'Order was successfully destroyed'
   end
-  
-  test "check routing number" do
+
+  test 'check routing number' do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
-
     click_on 'Add to Cart', match: :first
-
     click_on 'Checkout'
 
     fill_in 'order_name', with: 'Dave Thomas'
     fill_in 'order_address', with: '123 Main Street'
     fill_in 'order_email', with: 'dave@example.com'
 
-    assert_no_selector "#order_routing_number"
+    assert_no_selector '#order_routing_number'
 
     select 'Check', from: 'Pay type'
 
-    assert_selector "#order_routing_number"
+    assert_selector '#order_routing_number'
+
+    fill_in 'Routing #', with: '123456'
+    fill_in 'Account #', with: '987654'
+
+    perform_enqueued_jobs do
+      click_button 'Place Order'
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = Order.first
+    assert_equal 'Dave Thomas', order.name
+    assert_equal '123 Main Street', order.address
+    assert_equal 'Check', order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['dave@example.com'], mail.to
   end
 
   test 'check account number' do
@@ -48,11 +72,11 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_address', with: '123 Main Street'
     fill_in 'order_email', with: 'dave@example.com'
 
-    assert_no_selector "#order_account_number"
+    assert_no_selector '#order_account_number'
 
     select 'Check', from: 'Pay type'
 
-    assert_selector "#order_account_number"
+    assert_selector '#order_account_number'
   end
 
   test 'check credit card number' do
@@ -66,11 +90,11 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_address', with: '123 Main Street'
     fill_in 'order_email', with: 'dave@example.com'
 
-    assert_no_selector "#order_credit_card_number"
+    assert_no_selector '#order_credit_card_number'
 
     select 'Credit card', from: 'Pay type'
 
-    assert_selector "#order_credit_card_number"
+    assert_selector '#order_credit_card_number'
   end
 
   test 'check credit card expiration date' do
@@ -84,10 +108,10 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_address', with: '123 Main Street'
     fill_in 'order_email', with: 'dave@example.com'
 
-    assert_no_selector "#order_expiration_date"
+    assert_no_selector '#order_expiration_date'
 
     select 'Credit card', from: 'Pay type'
 
-    assert_selector "#order_expiration_date"
+    assert_selector '#order_expiration_date'
   end
 end
